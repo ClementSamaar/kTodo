3bfcfb69ce60bd1fce7f4b122327f91dc202ef94
Samaar Clément <clement.samaar@etu.univ-amu.fr>
2024-10-16 14:03:20 +0200

    init

---
+++ kTodo_back/src/main/java/com/ktodo_back/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ktodo_back;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
